48d285dacb22fbc8f2abd38398d6c1d9b3ad8f9d
Adrian <ADRIAN.QUIROSGARITA@@UCR.AC.CR>
2024-10-31 16:09:04 -0600

    Lab finalizado

--- Lab3/VentanaPrincipal.java
+++ Lab3/VentanaPrincipal.java
@@ -1,10 +1,8 @@
-import java.awt.event.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.*;
 
 public class VentanaPrincipal extends JFrame {
-
     public VentanaPrincipal() {
         setTitle("Formulario de Usuario");
         setSize(900, 800);
@@ -38,60 +36,72 @@ public class VentanaPrincipal extends JFrame {
         JLabel etiquetaTelefono = new JLabel("Telefono");
         JTextField campoTelefono = new JTextField(20);
 
-        panelFormulario.add(etiquetaNombre);
-        panelFormulario.add(campoNombre);
-        panelFormulario.add(etiquetaEmail);
-        panelFormulario.add(campoEmail);
-        panelFormulario.add(etiquetaTelefono);
-        panelFormulario.add(campoTelefono);
-        
-
         JButton botonGuardar = new JButton("Guardar");
+        botonGuardar.setEnabled(false);
+
+        JButton botonLimpiar = new JButton("Limpiar");
+        botonLimpiar.setEnabled(true);
         
         JLabel comboBoxLabel = new JLabel("Tipo de usuario: ");
-        String[] tiposDeUsuario = {"Cliente", "Empleado", "Administrador"};
+        String[] tiposDeUsuario = {"Seleccione", "Cliente", "Empleado", "Administrador"};
         JComboBox<String> tiposDeUsuarioComboBox = new JComboBox<String>(tiposDeUsuario);
-        panelFormulario.add(comboBoxLabel);
-        panelFormulario.add(tiposDeUsuarioComboBox);
 
         JLabel checkboxLabel = new JLabel("Terminos y condiciones:");
         JCheckBox terms = new JCheckBox ("Acepta terminos y condiciones");
         terms.setBounds(150, 100, 50, 50);
-        panelFormulario.add(checkboxLabel);
-        panelFormulario.add(terms);
-        add(panelFormulario);
         setVisible(true);
+
         terms.addActionListener(e -> {
-            if (terms.isSelected()) {
-                JOptionPane.showMessageDialog(null, "Datos guardados exitosamente.");
-            }
+            boolean seleccionoUnTipo = tiposDeUsuarioComboBox.getSelectedIndex() > 0;
+            botonGuardar.setEnabled(terms.isSelected() && seleccionoUnTipo);
+
         });
+        
+        tiposDeUsuarioComboBox.addActionListener(e ->{
+            boolean seleccionoUnTipo = tiposDeUsuarioComboBox.getSelectedIndex() > 0;
+            botonGuardar.setEnabled(terms.isSelected() && seleccionoUnTipo);
+        });
+
         botonGuardar.addActionListener(e -> {
             String nombre = campoNombre.getText();
             String email = campoEmail.getText();
             String telefono = campoTelefono.getText();
-
+            
             try {
                 FileWriter writer = new FileWriter("datos_usuario.txt", true);
-                writer.write("Nombre: " + nombre + ", Email: " + email + ", Telefono: " + telefono + "\n");
+                writer.write("\nNombre: " + nombre + ", \nEmail: " + email + ", \nTelefono: " + telefono);
                 writer.close();
                 JOptionPane.showMessageDialog(null, "Datos guardados exitosamente.");
             } catch (IOException ex) {
                 JOptionPane.showMessageDialog(null, "Error al guardar los datos.");
             }
         });
+        botonLimpiar.addActionListener(e -> {
+            campoNombre.setText("");
+            campoEmail.setText("");
+            campoTelefono.setText("");
+            tiposDeUsuarioComboBox.setSelectedIndex(0);
+            terms.setSelected(false);
+            botonGuardar.setEnabled(false);
+        });
+        panelFormulario.add(comboBoxLabel);
+        panelFormulario.add(tiposDeUsuarioComboBox);
+        panelFormulario.add(etiquetaNombre);
+        panelFormulario.add(campoNombre);
+        panelFormulario.add(etiquetaEmail);
+        panelFormulario.add(campoEmail);
+        panelFormulario.add(etiquetaTelefono);
+        panelFormulario.add(campoTelefono);
+        panelFormulario.add(checkboxLabel);
+        panelFormulario.add(terms);
         panelFormulario.add(botonGuardar);
-
-        
-        
-    
-
-
+        panelFormulario.add(botonLimpiar);
+        add(panelFormulario);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new VentanaPrincipal());
-        //ventana.setVisible(true);// Mostrar la ventana
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
 
     }
 }
